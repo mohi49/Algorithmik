e23e72bff93658e32fe3a3c77d75c1483829c7e1
mohamad.hamza <mohamad.hamza@A202-TC-12.ad.maxe-del.de>
2025-09-02 11:59:30 +0200

    Create project

---
+++ Algorithmik/src/main/java/me/mohamed/algorithmik/Algorithmik.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.mohamed.algorithmik;
+
+/**
+ *
+ * @author mohamad.hamza
+ */
+public class Algorithmik {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
